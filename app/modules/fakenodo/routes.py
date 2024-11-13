c4f3dc0bbf9fb2e9c1b57f5197419a84027ad3c2
Jose Manuel Miret Martin <miret.martin.josema@gmail.com>
2024-11-13 17:08:40 +0100

    fix(fakenodo): corrección en los ID's detectados por codacy en el archivo de rutas de fakenodo

--- app/modules/fakenodo/routes.py
+++ app/modules/fakenodo/routes.py
@@ -1,6 +1,5 @@
 from flask import jsonify, make_response
 from app.modules.fakenodo import fakenodo_bp
-
 base_url = "/fakenodo/api"
 
 
@@ -16,38 +15,38 @@ def create_fakenodo():
     return make_response(jsonify(response), 201)
 
 
-@fakenodo_bp.route(base_url + "/<id>/deposition", methods=["POST"])
-def deposition_files_fakenodo(id):
+@fakenodo_bp.route(base_url + "/<deposition_id>/deposition", methods=["POST"])
+def deposition_files_fakenodo(deposition_id):
     response = {
         "status": "success",
-        "message": f"The deposition {id} was successfully created",
+        "message": f"The deposition {deposition_id} was successfully created",
     }
     return make_response(jsonify(response), 201)
 
 
-@fakenodo_bp.route(base_url + "/<id>", methods=["DELETE"])
-def delete_deposition_fakenodo(id):
+@fakenodo_bp.route(base_url + "/<deposition_id>", methods=["DELETE"])
+def delete_deposition_fakenodo(deposition_id):
     response = {
         "status": "success",
-        "message": f"The deposition {id} was succesfully deleted",
+        "message": f"The deposition {deposition_id} was successfully deleted",
     }
     return make_response(jsonify(response), 200)
 
 
-@fakenodo_bp.route(base_url + "/<id>/resources/submit", methods=["POST"])
-def publish_deposition_fakenodo(id):
+@fakenodo_bp.route(base_url + "/<deposition_id>/resources/submit", methods=["POST"])
+def publish_deposition_fakenodo(deposition_id):
     response = {
         "status": "success",
-        "message": f"Deposition with ID {id} succesfully published in the API of Fakenodo",
+        "message": f"Deposition with ID {deposition_id} successfully published in the API of Fakenodo",
     }
     return make_response(jsonify(response), 202)
 
 
-@fakenodo_bp.route(base_url + "/<id>", methods=["GET"])
-def get_deposition_fakenodo(id):
+@fakenodo_bp.route(base_url + "/<deposition_id>", methods=["GET"])
+def get_deposition_fakenodo(deposition_id):
     response = {
         "status": "success",
-        "message": f"The deposition with ID {id} successfully gotten in the API of Fakenodo",
+        "message": f"The deposition with ID {deposition_id} successfully retrieved from the API of Fakenodo",
         "doi": "10.5072/fakenodo.123456",
     }
     return make_response(jsonify(response), 200)
