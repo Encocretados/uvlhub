9609b5a24679ece94654919891261e2dcaa5a4ca
alenicbra <alenicbra@alum.us.es>
2024-11-13 15:05:29 +0100

    feat(auth, community, dataset): cambio de modelo(user-community-dataset)

--- app/modules/dataset/models.py
+++ app/modules/dataset/models.py
@@ -70,7 +70,8 @@ class DSMetaData(db.Model):
 
 class DataSet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
 
     ds_meta_data_id = db.Column(db.Integer, db.ForeignKey('ds_meta_data.id'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
