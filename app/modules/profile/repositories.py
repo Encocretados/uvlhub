5510d346d1f7e36634b950d2a0e117548a64f0cf
Victoria Ruiz Delgado <ruizvictoria10@gmail.com>
2024-11-22 23:19:30 +0100

    feat: Ajustar repositories para obtener el perfil

--- app/modules/profile/repositories.py
+++ app/modules/profile/repositories.py
@@ -1,7 +1,16 @@
 from app.modules.profile.models import UserProfile
 from core.repositories.BaseRepository import BaseRepository
+from app.modules.auth.services import AuthenticationService
+
 
 
 class UserProfileRepository(BaseRepository):
     def __init__(self):
         super().__init__(UserProfile)
+
+    
+    def get_user_profile():
+       user = AuthenticationService().get_authenticated_user()   
+       if user:
+        return user.profile   
+       return None
