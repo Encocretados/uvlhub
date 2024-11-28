1a520e9754c3c34be32236d281bc13f0c900df67
Jose Manuel Miret Martin <miret.martin.josema@gmail.com>
2024-11-28 19:31:48 +0100

    refactor(fakenodo): modificaciones en el modelo, el repositorio y los seeders del fakenodo

--- app/modules/fakenodo/repositories.py
+++ app/modules/fakenodo/repositories.py
@@ -1,7 +1,10 @@
-from app.modules.fakenodo.models import Fakenodo
+from app.modules.fakenodo.models import Deposition
 from core.repositories.BaseRepository import BaseRepository
 
 
-class FakenodoRepository(BaseRepository):
+class DepositionRepo(BaseRepository):
     def __init__(self):
-        super().__init__(Fakenodo)
+        super().__init__(Deposition)
+
+    def create_new_deposition(self, metadata):
+        return self.create(metadata=metadata)
