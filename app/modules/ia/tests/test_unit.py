a9424bdbf739191f7845929093388150e116e838
Alvaro Martin <alvmarmun1@alum.us.es>
2024-12-13 13:17:18 +0100

    feat(ia): test para user_id vacio

--- app/modules/ia/tests/test_unit.py
+++ app/modules/ia/tests/test_unit.py
@@ -102,3 +102,17 @@ def test_ia_service_empty_question(mock_sessions_client, app, client):
         assert "error" in data
         assert data["error"] == "El campo 'question' no puede estar vacío."
 
+@patch("app.modules.ia.services.dialogflow.SessionsClient")
+def test_ia_service_empty_user_id(mock_sessions_client, app, client):
+    """Prueba el caso cuando el campo 'user_id' está vacío."""
+    
+    ia_service = IaService()
+
+    with app.test_request_context(json={"question": "Hola", "user_id": ""}):  # user_id vacío
+        response, status_code = ia_service.ia_service()
+        data = response.get_json()
+
+        assert status_code == 400
+        assert "error" in data
+        assert data["error"] == "Faltan parámetros necesarios."
+
