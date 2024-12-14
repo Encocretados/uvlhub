78a0c95e1c08f131bea6554b6bd6642a94b09f54
Celia aguilera Camino <wayacel@gmail.com>
2024-12-14 10:29:14 +0100

    fix(dataset): errores codacy

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -1,11 +1,8 @@
 import os
 import time
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
