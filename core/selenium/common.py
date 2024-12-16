3df4eca35b464373fc5db0438dc160996bbed468
José Manuel Miret Martín <josmirmar2@alum.us.es>
2024-12-16 19:17:36 +0100

    fix(selenium): deshacer cambios del chromedriver

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,5 +1,7 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def initialize_driver():
@@ -7,7 +9,10 @@ def initialize_driver():
     options = webdriver.ChromeOptions()
 
     # Initialise the browser using WebDriver Manager
-    service = Service('/usr/bin/chromedriver')
+    # service = Service(ChromeDriverManager().install())
+    driver_path = ChromeDriverManager().install()
+    chromedriver_binary = os.path.join(os.path.dirname(driver_path), 'chromedriver')
+    service = Service(chromedriver_binary)
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
